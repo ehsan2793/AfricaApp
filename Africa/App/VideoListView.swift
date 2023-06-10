bce2d2a2346801ed76dbe455282785db182e1542
ehsan2793 <ehsan2793@gmail.com>
2023-06-10 11:45:34 -0700

    a

---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Ehsan Rahimi on 6/10/23.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
