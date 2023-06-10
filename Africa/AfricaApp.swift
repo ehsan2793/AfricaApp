a478b0dc987261761bc0a739274c26d4532dd747
ehsan2793 <ehsan2793@gmail.com>
2023-06-10 03:58:59 -0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Ehsan Rahimi on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
