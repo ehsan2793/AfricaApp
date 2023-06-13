03f5c1aa8355c74e9263432d26cabcc8814ff2bc
ehsan2793 <ehsan2793@gmail.com>
2023-06-13 16:39:47 -0700

    fix indent

--- Africa/Model/VideoModel.swift
+++ Africa/Model/VideoModel.swift
@@ -11,9 +11,9 @@ struct Video: Hashable, Codable, Identifiable {
     let id: String
     let name: String
     let headline: String
-    
+
     // Computed Property
-    
+
     var thumnail: String {
         "video-\(id)"
     }
