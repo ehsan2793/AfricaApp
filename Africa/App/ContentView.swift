048625126addc17e5a20ce10187d60e71cf308ea
ehsan2793 <ehsan2793@gmail.com>
2023-06-16 21:06:56 -0700

    new grid button

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -11,27 +11,66 @@ struct ContentView: View {
     // MARK: - PROPERTIES
 
     let animals: [Animal]
+    let heptics = UIImpactFeedbackGenerator(style: .medium)
+    @State private var isGridViewActive: Bool = false
 
     // MARK: - BODY
 
     var body: some View {
         NavigationView {
-            ScrollView(showsIndicators: false) {
-                CoverImageView(coverImages: coverImages)
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            Group {
+                if !isGridViewActive {
+                    ScrollView(showsIndicators: false) {
+                        CoverImageView(coverImages: coverImages)
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
 
-                ForEach(animals) { animal in
-                    NavigationLink(
-                        destination: AnimalDetailView(animal: animal),
-                        label: {
-                            AnimalListItemView(animal: animal)
-                        }
-                    ) //: LINK
-                } //: LOOP
-            } //: SCROLL
+                        ForEach(animals) { animal in
+                            NavigationLink(
+                                destination: AnimalDetailView(animal: animal),
+                                label: {
+                                    AnimalListItemView(animal: animal)
+                                }
+                            ) //: LINK
+                        } //: LOOP
+                    } //: SCROLL
+                } else {
+                    Text("grid view is active")
+                } //: CONDITION
+            } //: GROUP
             .navigationTitle("Africa")
             .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // LIST
+                        Button(
+                            action: {
+                                print("list view")
+                                isGridViewActive = false
+                                heptics.impactOccurred()
+                            }, label: {
+                                Image(systemName: "square.fill.text.grid.1x2")
+                                    .font(.title2)
+                                    .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                            })
+
+                        // GRID
+                        Button(
+                            action: {
+                                print("grid view")
+                                heptics.impactOccurred()
+                                isGridViewActive = true
+                            },
+                            label: {
+                                Image(systemName: "square.grid.2x2")
+                                    .font(.title2)
+                                    .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                            }
+                        )
+                    } //: HSTACK
+                } //: BUTTONS
+            } //: TOOLBAR
         } //: NAVIGATION
     }
 }
